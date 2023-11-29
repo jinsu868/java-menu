1c44d0c0218cd71326b78a7a034f77b0714c2309
jinsu868 <mjinsu098@gmail.com>
2023-11-29 15:14:55 +0900

    feat(view): 싫어하는 음식 입력받는 기능 구현

--- src/main/java/menu/domain/DislikeMenu.java
+++ src/main/java/menu/domain/DislikeMenu.java
@@ -6,9 +6,13 @@ import java.util.Set;
 import menu.error.ErrorCode;
 
 public class DislikeMenu {
+    private static final int MIN_SIZE = 0;
+    private static final int MAX_SIZE = 2;
+
     private List<String> names;
 
     public DislikeMenu(List<String> names) {
+        validateLength(names);
         validateDuplication(names);
         this.names = names;
     }
@@ -17,6 +21,12 @@ public class DislikeMenu {
         Set<String> nameChecker = new HashSet<>();
         if (nameChecker.contains(names)) {
             throw ErrorCode.DUPLICATE_DISLIKE_MENU_NAME.getException();
+        }
+    }
+
+    private void validateLength(List<String> names) {
+        if (names.size() < MIN_SIZE || names.size() > MAX_SIZE) {
+            throw ErrorCode.INVALID_DISLIKE_MENU_SIZE.getException();
         }
     }
 }
